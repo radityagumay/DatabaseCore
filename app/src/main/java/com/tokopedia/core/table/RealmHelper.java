6871941060d25750ae295b56dce30902df7a2be5
R Aditya Gumay <gumay.raditya@yahoo.com>
2016-02-22 15:53:55 +0700

    first commit

---
+++ app/src/main/java/com/tokopedia/core/table/RealmHelper.java
@@ -0,0 +1,17 @@
+package com.tokopedia.core.table;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.realm.Realm;
+
+/**
+ * Created by raditya.gumay on 22/02/2016.
+ */
+public class RealmHelper extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
